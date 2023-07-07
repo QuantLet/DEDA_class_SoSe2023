33872b45659991565db694f25965137f1aa17e57
bachanasashvili <bachana.anasashvili@student.hu-berlin.de>
2023-07-07 16:59:46 +0200

    Added en_detect
    Added en_detect function to the lang_detect module to address file-specific issues we encountered.

--- DEDA_class_SoSe2023_LDA_MSc_Theses/lang_detect.py
+++ DEDA_class_SoSe2023_LDA_MSc_Theses/lang_detect.py
@@ -46,4 +46,48 @@ def de_detect(folder = None, theses = None):
     
     return german_list
 
+  
     
+def en_detect(folder = None, theses = None):
+    '''
+    Goes through the scraped and downloaded PDF files and scans them to see if they are in English. 
+    This is necessary because some of the documents are in German (and some are unreadable).
+    To save memory, automatically selected to check pages 8, 12, 16, 20, 24; which should in all likelihood cases be body of text.
+    '''
+    print(f'\nCurrent working directory: {os.getcwd()}')
+
+    # List to store German thesis filenames
+    english_list = []
+    
+    for thesis in theses:
+        
+        with pdfplumber.open(os.path.join(folder, thesis), pages=[8, 12, 16, 20, 24]) as pdf:
+            
+            print(f'\nCurrently working on {thesis}.')
+            
+            # Initial condition needed for removing those identified as german
+            english = False
+            for page in pdf.pages:
+                
+                text = page.extract_text()
+                
+                # Try condition necessary as detect() function breaks down on empty pages
+                try:
+                    if detect(text) == 'en':
+                        print(f'\n{thesis} was identified as English.')
+                        english_list.append(thesis)
+                        english = True
+                        break 
+                except:
+                    continue
+            
+            if not english:
+                pdf.close()
+                os.remove(os.path.join(folder,thesis))
+                print(f'\n{thesis} removed from directory')
+                    
+
+    print(f'\nWe have identified in total {len(english_list)} English language documents. See below the full list:')
+    print(english_list)
+    
+    return english_list    
