2a1a1abc59fcb46fee63359ed25709bc5f105d9e
Antonio Aquiles Alvarez Aguilar <aaguilar@kayak.com>
2023-07-16 14:06:20 +0200

    Reddit WSB Sentiment - Last changes added before grading. Shell file for reproducing added

---
+++ DEDA_class_SoSe2023_Reddit_WSB_Sentiment/Reddit_WSB_Sentiment.sh
@@ -0,0 +1,12 @@
+# MAKE SURE TO CORRECTLY SET UP A PYTHON VIRTUAL ENVIRONMENT, INSTALL requirements.txt
+# AND DOWNLOAD THE KAGGLE POSTS DATA IN README BEFORE RUNNING ANYTHING!
+cd src
+
+# If your internet connection is unstable, make sure to change the last line of the file below
+# to reflect the last posts you were able to scrape. The last posts id is the name in the logs when it fails or inside the
+# data/comments directory
+python 00-get_posts_comments.py
+
+python 01-join-all-comments.py
+python 02-sentiment-output.py
+python 03-results.py
