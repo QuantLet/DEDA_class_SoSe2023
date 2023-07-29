24886787c8b0eead814fc90438dba7155f1a1abc
Timur Tenishev <timur.tenishev@student.hu-berlin.de>
2023-07-29 03:47:22 +0200

    UMAP for documents added

--- DEDA_class_SoSe2023_LDA_Theses/DEDA_class_SoSe2023_LDA_MSc_Theses/LDA_MSc_3_Making_Corpus/corpus_maker.py
+++ DEDA_class_SoSe2023_LDA_Theses/DEDA_class_SoSe2023_LDA_MSc_Theses/LDA_MSc_3_Making_Corpus/corpus_maker.py
@@ -3,6 +3,7 @@ import re
 import pandas as pd
 import pickle
 from gensim import corpora
+from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,6 +25,7 @@ class CorpusMaker:
     
     Args:
         input_folder (str): The folder containing the filtered MSc theses. (Should set to the output of the preprocessing function)
+        info file (str): path to the pickle file containing theses titles and authors. (Should set to thesis_info.pkl from LDA_MSc_Webscraping)
         
     Returns:
         dictionary: A dictionary generated using Gensim.
@@ -36,17 +38,20 @@ class CorpusMaker:
         make_corpus: Processes the filtered theses texts, drops rare words overall, creates corpus.
         show_top_words: Prints the most frequent words per thesis in the corpus.
         make_wordcloud: Generates a wordcloud image from the corpus.
-        make_UMAP: applies UMAP to term frequencies for dimensionality reduction and visualizes as interactive 2d graph.
+        make_word_UMAP: applies UMAP to terms embeddings (word2vec) for dimensionality reduction and visualizes as interactive 2d graph.
+        make_theses_UMAP: applies UMAP to theses (TF-IDF) for dimensionality reduction and visualizes as interactive 2d graph.
     '''
     
     
-    def __init__(self, input_folder):
+    def __init__(self, input_folder, info_file):
         self.input_folder = input_folder
+        self.info_file = info_file
         self.dictionary = None
         self.dictionary_token2id = None
         self.corpus = None
         self.texts = None
         self.dates = None
+        self.sorted_thesis_info = None
 
     def make_corpus(self):
         '''
@@ -59,6 +64,9 @@ class CorpusMaker:
 
         print('\nCreating corpus...')
 
+        with open(self.info_file, 'rb') as file:
+            thesis_info = pickle.load(file)
+        
         theses = os.listdir(self.input_folder)
         
         #sorts theses according to their date of publishing
@@ -73,23 +81,30 @@ class CorpusMaker:
 
         theses = list(sorted_dates_dict.keys())
         
+        #sort thesis_info accordingly
+        sorted_thesis_info = {}
+        for thesis in theses:
+            sorted_thesis_info[thesis] = thesis_info[thesis[9:-4]]
+        
         # Load and tokenize filtered documents
 
         # Initialize empty list
         filtered_theses = []
 
         # Iterate over thesis in theses
+        cwd = os.getcwd()
+        os.chdir(self.input_folder)
         for thesis in theses:
-
             # Open file in read mode
-            with open(os.path.join(self.input_folder, thesis), 'r') as file:
+            with open(thesis, 'r') as file:
 
                 # Define filtered thesis as the file.read
                 filtered_thesis = file.read()
 
                 # Append to the filtered_theses list
                 filtered_theses.append(filtered_thesis)
-
+        os.chdir(cwd)
+        
         # Tokenize:
 
         # Get a list of lists that includes tokens for every thesis 
@@ -121,6 +136,8 @@ class CorpusMaker:
         self.dates = list(sorted_dates_dict.values())
         # Pickle the data and save into output folder
         
+        self.sorted_thesis_info = sorted_thesis_info
+        
         # Set up folder
         if not os.path.exists('DICT_CORP'):
             os.makedirs('DICT_CORP')
@@ -144,10 +161,13 @@ class CorpusMaker:
         # Save dates
         with open(os.path.join('DICT_CORP', 'dates.pkl'), 'wb') as file:
             pickle.dump(self.dates, file)
+            
+        with open(os.path.join('DICT_CORP','sorted_thesis_info.pkl'), 'wb') as file:
+            pickle.dump(self.sorted_thesis_info, file)
         
         print('\nCorpus succesfully created.')
 
-        return self.dictionary, self.dictionary_token2id, self.corpus, self.texts, self.dates
+        return self.dictionary, self.dictionary_token2id, self.corpus, self.texts, self.dates, self.sorted_thesis_info
 
 
     def show_top_words(self, amount):
@@ -188,37 +208,86 @@ class CorpusMaker:
         plt.show()
         plt.close()
         
-    def make_UMAP(self):
+    def make_word_UMAP(self):
         '''
-        Small function to generate UMAP visualization of terms distribution
+        Small method to generate UMAP visualization of terms distribution
         '''
         
-        docs = [" ".join(text) for text in self.texts]
+        # Train a Word2Vec model
+        model = Word2Vec(self.texts, vector_size=100, window=5, min_count=1, workers=4)
 
-        # Create an instance of TfidfVectorizer
-        vectorizer = TfidfVectorizer()
+        # Create a list of words and corresponding vectors
+        words = pd.DataFrame({"term": model.wv.index_to_key})
+        vectors = model.wv.vectors
         
+        umap_embedding = UMAP(random_state = 66).fit(vectors)
+        print(f'Shape after UMAP: {umap_embedding.embedding_.shape}')
+        
+        fig = umap.plot.interactive(umap_embedding, hover_data = words, point_size=3)
+        show(fig)
+
+        output_file("UMAP words interactive.html")  # Set the output file path
+        save(fig)
+
+        f = umap.plot.points(umap_embedding)
+        f.set_title('UMAP projection of {} unique words'.format(len(words)), fontsize = 24)
+        plt.savefig('UMAP terms.png', transparent = True, dpi = 300)
+        plt.show()
+        plt.close()
+        
+        plt.figure(figsize=(19.2, 14.4))
+        plt.scatter(umap_embedding.embedding_[:, 0], umap_embedding.embedding_[:, 1], alpha=0.5, s=10)
+        for i, word in enumerate(words['term']):
+            plt.annotate(word, xy=(umap_embedding.embedding_[i, 0], umap_embedding.embedding_[i, 1]), fontsize=8)
+        plt.grid(False)
+        plt.savefig('UMAP terms annotated.png', transparent = True, dpi = 300)
+        plt.show()
+        plt.close()
+        
+    def make_theses_UMAP(self):
+        '''
+        Generates UMAP visualization of documents distribution
+        '''
+        thesis_info_pd = pd.DataFrame(self.sorted_thesis_info).T
+        thesis_info_pd.columns = ['Title', 'Author']
+
+        thesis_info_pd = thesis_info_pd.reset_index()[['Title', 'Author']]
+
+        delete_dupl = list(thesis_info_pd[thesis_info_pd.duplicated() == True].index)
+        delete_dupl.reverse()
+        delete_dupl
+
+        thesis_info_pd = thesis_info_pd.drop_duplicates()
+
+        docs = [" ".join(thesis) for thesis in self.texts]
+        len_docs = len(docs)
+
+        for ind in delete_dupl:
+            del docs[ind]
+
+        vectorizer = TfidfVectorizer()
+
         # Fit and transform the corpus using TF-IDF vectorization
         tfidf_matrix = vectorizer.fit_transform(docs)
 
-        # Convert the TF-IDF matrix to an array for extracting feature names
-        tfidf_array = tfidf_matrix.toarray()
-        # Get the feature names (terms)
-        feature_names = vectorizer.get_feature_names_out()
-        hover_data = pd.DataFrame({'index':np.arange(len(feature_names)),
-                                   'term':feature_names})
-
-        #transpose for plotting
-        tfidf_matrix = tfidf_matrix.T
-        print(f'Shape of the matrix: {tfidf_matrix.shape}')
-
         # Apply UMAP and plot results
-        tfidf_embedding = UMAP(metric='hellinger', random_state = 66).fit(tfidf_matrix)
-        print(f'Shape after UMAP: {tfidf_embedding.embedding_.shape}')
-        
-        fig = umap.plot.interactive(tfidf_embedding, hover_data = hover_data, point_size=1)
-        show(fig)
+        mapper = UMAP(random_state=66).fit(tfidf_matrix)
+        f = umap.plot.points(mapper)
+        f.set_title('UMAP projection of {} MSc theses'.format(len_docs), fontsize = 24)
+        plt.savefig('UMAP theses.png', transparent = True, dpi = 300)
+        plt.show()
+        plt.close()
 
-        output_file("UMAP interactive.html")  # Set the output file path
+        fig = umap.plot.interactive(mapper, hover_data = thesis_info_pd, point_size=5)
+        show(fig)
+        output_file("UMAP theses interactive.html")  # Set the output file path
         save(fig)
-        
+
+        plt.figure(figsize=(19.2, 14.4))
+        plt.scatter(mapper.embedding_[:, 0], mapper.embedding_[:, 1], alpha=0.5, s=100)
+        for i, title in enumerate(thesis_info_pd['Title']):
+            plt.annotate(title, xy=(mapper.embedding_[i, 0], mapper.embedding_[i, 1]), fontsize=24)
+        plt.grid(False)
+        plt.savefig('UMAP theses annotated.png', transparent = True, dpi = 300)
+        plt.show()
+        plt.close()
